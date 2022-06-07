9837f63c4bc629034b664f4530f8680f90b0dcee
Borja Montseny <68342939+borjaMontseny@users.noreply.github.com>
2022-06-07 09:48:18 +0200

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'LoginAndRegister';
+  constructor() { }
+
+
+  ngOnInit(): void {
+
+    const sign_in_btn: any = document.querySelector("#sign-in-btn");
+    const sign_up_btn: any = document.querySelector("#sign-up-btn");
+    const container: any = document.querySelector(".container");
+
+    sign_up_btn.addEventListener("click", () => {
+      container.classList.add("sign-up-mode");
+    });
+
+    sign_in_btn.addEventListener("click", () => {
+      container.classList.remove("sign-up-mode");
+    });
+
+  }
 }
+
+
+
+
+
+
